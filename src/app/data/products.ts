2e2c2ceb1566a060a8191789e0eddad56ed90a85
DsBm2468 <dsbenavides@uniboyaca.edu.co>
2025-09-16 18:30:06 -0500

    Se le agregaron imagenes de los productos, además se le hizo un leve ajuste en el scss de estas imagenes

--- src/app/data/products.ts
+++ src/app/data/products.ts
@@ -1,7 +1,7 @@
 import { Product } from "../models/product";
 
 export const PRODUCTS: Product[] = [
-  { id: 1, name: 'Laptop', price: 3500, image: 'https://via.placeholder.com/300', description: 'Laptop potente' },
-  { id: 2, name: 'Smartphone', price: 1200, image: 'https://via.placeholder.com/300', description: 'Teléfono moderno' },
-  { id: 3, name: 'Audífonos', price: 300, image: 'https://via.placeholder.com/300', description: 'Audio de calidad' }
+  { id: 1, name: 'Laptop', price: 3500, image: 'https://images.unsplash.com/photo-1511385348-a52b4a160dc2?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwY29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D', description: 'Laptop potente' },
+  { id: 2, name: 'Smartphone', price: 1200, image: 'https://i5-mx.walmartimages.com/mg/gm/1p/images/product-images/img_large/00822248508458l.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF', description: 'Teléfono moderno' },
+  { id: 3, name: 'Audífonos', price: 300, image: 'https://www.mipcparquecentral.com/cdn/shop/files/imagen-auriculares_g733_audifonos_inalambricos_con_microfono_azul-1.jpg?v=1709752195', description: 'Audio de calidad' }
 ];
